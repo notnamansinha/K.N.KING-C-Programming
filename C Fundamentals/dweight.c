5b85dc553c4546a9108e7a9c5c79e2ba5e906b81
Naman Kumar Sinha <notnamansinha@gmail.com>
2025-12-19 12:05:02 +0530

    Refactor dimensional weight calculation to use integers
    Updated the program to calculate volume and dimensional weight using integer types instead of float.

--- C Fundamentals/dweight.c
+++ C Fundamentals/dweight.c
@@ -3,19 +3,21 @@
  
 int main(void) 
 { 
-    float length, width, height, dim_weight;
+    int length, width, height, volume, dim_weight;
     // Get the dimensions of the box
     printf("Enter the length of the box (in cm): ");
-    scanf("%f", &length);
+    scanf("%d", &length);
     printf("Enter the width of the box (in cm): ");
-    scanf("%f", &width);
+    scanf("%d", &width);
     printf("Enter the height of the box (in cm): ");
-    scanf("%f", &height);
+    scanf("%d", &height);
 
     // Compute the dimensional weight
-    dim_weight = ((length * width * height)+165) / 166 ;
+    volume = (length * height * width);
+    dim_weight = (length * width * height + 165) / 166 ;
 
     // Display the result
-    printf("The dimensional weight of the box is: %.2f kg\n", dim_weight);
+    printf("The volume is: %d \n",volume);
+    printf("The dimensional weight of the box is: %d kg\n", dim_weight);
 }
 
