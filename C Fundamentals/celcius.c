8cdc6d1f87461d6e9649cbde35a7bc503bfaa4f4
Naman Kumar Sinha <notnamansinha@gmail.com>
2025-12-19 13:33:07 +0530

    Fix formatting and comments in celcius.c

--- C Fundamentals/celcius.c
+++ C Fundamentals/celcius.c
@@ -1,6 +1,7 @@
 // converts a farenheit temperature to celsius
 
 #include <stdio.h>
+// Using macro-definition below:
 #define FREEZING_PT 32.0f
 #define SCALE_FACTOR 5.0f / 9.0f
 
@@ -12,3 +13,4 @@ int main(void)
     celcius = (farenheit - FREEZING_PT) * SCALE_FACTOR;
     printf("Celcius Equivalent: %.1fC \n", celcius);
 }
+
